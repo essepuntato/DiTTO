fe2ee3394918a8f05e5da5b8fe5f8f3f0a11396a
six <six@n33d23.cs.unibo.it>
2014-03-26 16:44:59 +0100

    Extension of DiTTO with Graffoo transformations.

--- src/it/essepuntato/ditto/DittoServlet.java
+++ src/it/essepuntato/ditto/DittoServlet.java
@@ -3,13 +3,21 @@ package it.essepuntato.ditto;
 import it.essepuntato.ditto.MimeType;
 import it.essepuntato.ditto.SourceExtractor;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.io.StringReader;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -29,6 +37,7 @@ public class DittoServlet extends HttpServlet {
 	private String erMinimal;
 	private String erIntermediate;
 	private String erMaximal;
+	private String graffoo;
 	private int maxTentative = 3;
        
     /**
@@ -45,7 +54,7 @@ public class DittoServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		resolvePaths(request); /* Set all paths */
 		response.setCharacterEncoding("UTF-8");
-		PrintWriter out;
+		ServletOutputStream out = null;
 		
 		SourceExtractor extractor = new SourceExtractor(this);
 		extractor.addMimeTypes(MimeType.mimeTypes);
@@ -56,27 +65,76 @@ public class DittoServlet extends HttpServlet {
 				
 				URL diagramURL = new URL(stringURL);
 				String content = extractor.exec(diagramURL);
+				String type = request.getParameter("type");
 				
 				content = applyXSLTTransformation(
 						content, 
 						request.getParameter("ontology-url"),
 						request.getParameter("ontology-prefix"),
 						new Boolean(request.getParameter("look-across-for-labels")),
-						getXSLT(request.getParameter("type")));
+						new Boolean(request.getParameter("main-ontology-only")),
+						new Boolean(request.getParameter("version-iri-imported")),
+						getXSLT(type), type).trim();
 				
-				response.setContentType("application/rdf+xml");
-				response.setHeader("content-disposition", "attachment; filename=\"ontology.owl\"");
-				out = response.getWriter();
-				out.println(content);
+				if (type.equals("graffoo")) {
+					String[] tempOntologies = content.split("\r\r\r");
+					List<String> finalOntologies = new ArrayList<String>(); 
+					for (String tempOntology : tempOntologies) {
+						if (!tempOntology.replaceAll("\\s+", "").trim().equals("")) {
+							finalOntologies.add(tempOntology);
+						}
+					}
+					
+					if (finalOntologies.size() > 1) {
+						response.setContentType("application/zip"); /* ZIP file in case of multiple ontologies */
+						response.setHeader("content-disposition", "attachment; filename=\"ontologies.zip\"");
+						out = response.getOutputStream();
+						createZipFile(finalOntologies, out);
+						
+					} else {
+						response.setContentType("text/plain"); /* Manchester Syntax */
+						response.setHeader("content-disposition", "attachment; filename=\"ontology.owl\"");
+						out = response.getOutputStream();
+						out.write(content.getBytes());
+					}
+					
+				} else {
+					response.setContentType("application/rdf+xml");  /* RDF/XML */
+					response.setHeader("content-disposition", "attachment; filename=\"ontology.owl\"");
+					out = response.getOutputStream();
+					out.write(content.getBytes());
+				}
+				
+				out.flush();
 				i = maxTentative;
 			} catch (Exception e) {
 				if (i + 1 == maxTentative) {
 					response.setContentType("text/html");
-					out = response.getWriter();
+					//out = response.getWriter();
 					out.println(getErrorPage(e));
 				}
 			}
 		}
+	}
+	
+	private ByteArrayOutputStream createZipFile(List<String> list, OutputStream out) throws IOException {
+		ByteArrayOutputStream b = new ByteArrayOutputStream(); 
+		ZipOutputStream zip = new ZipOutputStream(out);
+		for (int i = 0; i < list.size(); i++) {
+			String ontology = list.get(i);
+			InputStream is = new ByteArrayInputStream(ontology.getBytes());
+			ZipEntry entry = new ZipEntry("ontology_" + (i+1) + ".owl");
+			zip.putNextEntry(entry);
+			int len;
+			byte[] buf = new byte[1024]; 
+			while ((len = is.read(buf)) > 0) {
+				zip.write(buf, 0, len);
+			}
+			zip.closeEntry();
+			is.close();
+		}
+		zip.close();
+		return b;
 	}
 
 	private String getXSLT(String parameter) {
@@ -89,6 +147,8 @@ public class DittoServlet extends HttpServlet {
 				result = erIntermediate;
 			} else if (parameter.equals("er-maximal")) {
 				result = erMaximal;
+			} else if (parameter.equals("graffoo")) {
+				result = graffoo;
 			}
 		}	
 		
@@ -97,7 +157,8 @@ public class DittoServlet extends HttpServlet {
 
 	private String applyXSLTTransformation(
 			String source, String ontologyURI, String ontologyPrefix, 
-			boolean lookAcrossForLabels, String xsltURL) 
+			boolean lookAcrossForLabels, boolean mainOntologyOnly, boolean versionIRIImported, 
+			String xsltURL, String type) 
 	throws TransformerException {	
 		TransformerFactory tfactory = new net.sf.saxon.TransformerFactoryImpl();
 		
@@ -107,9 +168,18 @@ public class DittoServlet extends HttpServlet {
 			tfactory.newTransformer(
 					new StreamSource(xsltURL));
 		
-		transformer.setParameter("ontology-prefix", ontologyPrefix);
-		transformer.setParameter("ontology-uri", ontologyURI);
-		transformer.setParameter("look-across-for-labels", lookAcrossForLabels);
+		if (type.equals("graffoo")) {
+			if (ontologyURI != null && !ontologyURI.equals("")) {
+				transformer.setParameter("default-empty-prefix-param", ontologyURI + "/");
+				transformer.setParameter("default-ontology-iri-param", ontologyURI);
+			}
+			transformer.setParameter("use-imported-ontology-version-iri-param", versionIRIImported);
+			transformer.setParameter("generate-all-ontologies-param", !mainOntologyOnly);
+		} else {
+			transformer.setParameter("ontology-prefix", ontologyPrefix);
+			transformer.setParameter("ontology-uri", ontologyURI);
+			transformer.setParameter("look-across-for-labels", lookAcrossForLabels);
+		}
 		
 		StreamSource inputSource = new StreamSource(new StringReader(source));
 		
@@ -124,6 +194,7 @@ public class DittoServlet extends HttpServlet {
 		erMinimal = getServletContext().getRealPath("er-minimal.xsl");
 		erIntermediate = getServletContext().getRealPath("er-intermediate.xsl");
 		erMaximal = getServletContext().getRealPath("er-maximal.xsl");
+		graffoo =  getServletContext().getRealPath("graffoo.xsl");
 	}
 	
 	private String getErrorPage(Exception e) {
